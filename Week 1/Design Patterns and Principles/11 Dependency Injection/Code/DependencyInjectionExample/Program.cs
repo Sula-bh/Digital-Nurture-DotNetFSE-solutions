d6c345fdf37c488a381a7d9948138e15657e2bc5
Sulabh Acharya <sulava06@gmail.com>
2026-06-22 11:13:01 +0530

    complete design pattern exercise 11

---
+++ Week 1/Design Patterns and Principles/11 Dependency Injection/Code/DependencyInjectionExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using DependencyInjectionExample.Repositories;
+using DependencyInjectionExample.Services;
+
+ICustomerRepository repository = new CustomerRepositoryImpl();
+
+CustomerService service = new CustomerService(repository);
+
+var customer = service.GetCustomer(101);
+
+Console.WriteLine($"ID: {customer.Id}");
+
+Console.WriteLine($"Name: {customer.Name}");
---
+++ Week 1/Design Patterns and Principles/11 Dependency Injection/Code/DependencyInjectionExample/Repositories/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+using DependencyInjectionExample.Models;
+
+namespace DependencyInjectionExample.Repositories;
+
+public interface ICustomerRepository
+{
+    Customer FindCustomerById(int id);
+}
