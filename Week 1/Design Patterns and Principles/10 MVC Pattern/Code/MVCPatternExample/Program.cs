d5a368c192d56abffc2e51c14287c02fc9ca376f
Sulabh Acharya <sulava06@gmail.com>
2026-06-22 10:57:45 +0530

    complete design pattern exercise 10

---
+++ Week 1/Design Patterns and Principles/10 MVC Pattern/Code/MVCPatternExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using MVCPatternExample.Controllers;
+using MVCPatternExample.Models;
+using MVCPatternExample.Views;
+
+Student model = new Student("Alice", 101, "A");
+
+StudentView view = new StudentView();
+
+StudentController controller = new StudentController(model, view);
+
+controller.UpdateView();
+
+Console.WriteLine();
+
+controller.SetStudentName("Bob");
+controller.SetStudentGrade("A+");
+
+controller.UpdateView();
