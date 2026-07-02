3c2f2c94ffa28e65b546d0e11d091f8da32512a3
Sulabh Acharya <sulabh.acharya17@gmail.com>
2026-07-02 21:23:25 +0530

    complete entity framework core lab 1-5

---
+++ Week 2/Entity Framework Core 8.0/RetailInventory/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=(localdb)\MSSQLLocalDB;
+              Database=RetailInventory;
+              Trusted_Connection=True;
+              TrustServerCertificate=True");
+    }
+}
---
+++ Week 2/Entity Framework Core 8.0/RetailInventory/Program.cs
@@ -0,0 +1,62 @@
+﻿using RetailInventory.Data;
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+using AppDbContext context = new();
+
+Category electronics = new()
+{
+    Name = "Electronics"
+};
+
+Category groceries = new()
+{
+    Name = "Groceries"
+};
+
+await context.Categories.AddRangeAsync(
+    electronics,
+    groceries);
+
+Product laptop = new()
+{
+    Name = "Laptop",
+    Price = 75000,
+    Category = electronics
+};
+
+Product riceBag = new()
+{
+    Name = "Rice Bag",
+    Price = 1200,
+    Category = groceries
+};
+
+await context.Products.AddRangeAsync(
+    laptop,
+    riceBag);
+
+await context.SaveChangesAsync();
+
+Console.WriteLine("Data inserted successfully.");
+
+List<Product> products =
+    await context.Products.ToListAsync();
+
+foreach (Product product in products)
+{
+    Console.WriteLine(
+        $"{product.Id} - {product.Name} - ₹{product.Price}");
+}
+
+Product? pro =
+    await context.Products.FindAsync(1);
+
+Console.WriteLine(pro?.Name);
+
+Product? expensive =
+    await context.Products
+        .FirstOrDefaultAsync(
+            p => p.Price > 50000);
+
+Console.WriteLine(expensive?.Name);
